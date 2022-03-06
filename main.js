27e0708dc40a798ecf85c77eda8d500fcd55370a
meow854 <81199224+meow854@users.noreply.github.com>
2022-03-06 16:13:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas= createCanvas(400, 300);
+    canvas.center();
+}
+
+function preload() {
+
+}
+
+function draw() {
+
+}
+
+function abc() {
+    save("filterpic.png");
+}
